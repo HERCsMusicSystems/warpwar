51fec11e1e6633d87e58693809e45f9c639edf4d
unknown <ext-wolfrob@ADLW0230.corp.knorr-bremse.com>
2014-03-19 14:49:23 +1030

    Graphics 2D moved to prolog source code.

--- code/boarder.h
+++ code/boarder.h
@@ -4,6 +4,7 @@
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include "graphics2d.h"
 
 #include "prolog.h"
 #include <gtk/gtk.h>
@@ -81,77 +82,11 @@
 #define SELECTION "Selection"
 #define IS_DECK "IsDeck?"
 
-class point;
-class rect;
-class colour;
 class boarder;
 class boarder_viewport;
 class boarder_token;
 class text_token;
 class rectangle_token;
-
-#define POINT(p) p . x, p . y
-
-class point {
-public:
-	double x, y;
-	point (double x, double y);
-	point (double locations [2]);
-	point (void);
-	point operator + (const point & p) const;
-	point operator - (const point & p) const;
-	point operator - (void) const;
-	point operator * (const double & scale) const;
-	point operator * (const point & p) const;
-	point operator / (const double & scale) const;
-	bool operator == (const point & p) const;
-	bool operator != (const point & p) const;
-	point operator += (const point & p);
-	point operator -= (const point & p);
-	point operator *= (const double & d);
-	point operator *= (const point & p);
-	point half (void);
-	point rotate (double angle);
-	point swap (void);
-	void round (void);
-	void minimise (void);
-	void maximise (void);
-};
-
-#define RECT(r) r . position . x, r . position . y, r . size . x, r . size . y
-class rect {
-public:
-	point position;
-	point size;
-	point centre (void);
-	point centre (double scaling);
-	bool overlap (rect area);
-	void positivise (void);
-	bool operator == (const rect & r) const;
-	bool operator != (const rect & r) const;
-	void minimise (void);
-	void maximise (void);
-	rect (point offset, point size);
-	rect (double x, double y, double width, double height);
-	rect (double locations [4]);
-	rect (void);
-};
-
-#define COLOUR(c) c . red, c . green, c . blue
-#define ACOLOUR(c) c . red, c . green, c . blue, c . alpha
-#define INTCOLOUR(c) colour_to_int (c . red), colour_to_int (c . green), colour_to_int (c . blue)
-#define AINTCOLOUR(c) colour_to_int (c . red), colour_to_int (c . green), colour_to_int (c . blue), colour_to_int (c . alpha)
-
-double int_to_colour (int c);
-int colour_to_int (double c);
-class colour {
-public:
-	double red, green, blue, alpha;
-	bool operator == (const colour & c) const;
-	bool operator != (const colour & c) const;
-	colour (int red, int green, int blue, int alpha = 255);
-	colour (void);
-};
 
 class boarder {
 public:
